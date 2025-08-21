edcc0342a73e394ad1946a973366b0a25bce4761
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2025-08-21 14:13:17 +0300

    Merge TimelineController into TweetsController

--- packages/server/src/tweets/tweets.controller.ts
+++ packages/server/src/tweets/tweets.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, HttpCode, HttpStatus, NotImplementedException, Post } from "@nestjs/common"
+import { Controller, Get, HttpCode, HttpStatus, NotImplementedException, Post } from "@nestjs/common"
 
 @Controller('tweets')
 export class TweetsController {
+  // TODO: (query) limit + offset
+  @Get()
+  async getTweets(): Promise<Api.HttpResponseBody<never>> {
+    throw new NotImplementedException()
+  }
+
   // TODO: (body) text
   @Post()
   @HttpCode(HttpStatus.CREATED)
--- packages/server/src/tweets/tweets.module.ts
+++ packages/server/src/tweets/tweets.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common"
 import { TweetsController } from "./tweets.controller.js"
-import { TimelineController } from "./timeline.controller.js"
 
 @Module({
   controllers: [
     TweetsController,
-    TimelineController,
   ],
 })
 export class TweetsModule { }
