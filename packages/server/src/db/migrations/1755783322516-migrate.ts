a31da2af800c001c653c92ffa372627d1745abf6
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2025-08-21 16:36:03 +0300

    Define `tweet-like-count` materialized view

---
+++ packages/server/src/db/migrations/1755783322516-migrate.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migrate1755783322516 implements MigrationInterface {
+    name = 'Migrate1755783322516'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE MATERIALIZED VIEW "tweet_like_count" AS SELECT "like"."tweet_id" AS "tweet_id", count(*) AS "like_count" FROM "like" "like" GROUP BY tweet_id`);
+        await queryRunner.query(`INSERT INTO "typeorm_metadata"("database", "schema", "table", "type", "name", "value") VALUES (DEFAULT, $1, DEFAULT, $2, $3, $4)`, ["public","MATERIALIZED_VIEW","tweet_like_count","SELECT \"like\".\"tweet_id\" AS \"tweet_id\", count(*) AS \"like_count\" FROM \"like\" \"like\" GROUP BY tweet_id"]);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DELETE FROM "typeorm_metadata" WHERE "type" = $1 AND "name" = $2 AND "schema" = $3`, ["MATERIALIZED_VIEW","tweet_like_count","public"]);
+        await queryRunner.query(`DROP MATERIALIZED VIEW "tweet_like_count"`);
+    }
+
+}
--- packages/server/src/tweets/tweet.entity.ts
+++ packages/server/src/tweets/tweet.entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
+import { Column, Entity, EventSubscriber, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
 import { User } from "@/users/user.entity.js"
+import { TweetLikeCountUpdater } from "./tweet-like-count-updater.abstract.js"
 
 @Entity('tweet')
 export class Tweet {
@@ -36,4 +37,11 @@ export class Tweet {
     default: () => 'CURRENT_TIMESTAMP',
   })
   readonly createdAt!: Date
+}
+
+@EventSubscriber()
+export class TweetSubscriber extends TweetLikeCountUpdater<Tweet> {
+  override listenTo() {
+    return Tweet
+  }
 }
