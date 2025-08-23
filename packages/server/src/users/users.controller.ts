bcf2a95ee9d49d9cc2436e0ddfe4595c2713f590
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2025-08-23 15:27:36 +0300

    Little things

--- packages/server/src/users/users.controller.ts
+++ packages/server/src/users/users.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, NotImplementedException, Post, Redirect, Version } from "@nestjs/common"
+import { Open } from "@/auth/open.decorator.js"
 
 @Controller('users')
 export class UsersController {
+  @Open()
   @Post()
   @Version('1')
   @Redirect('/v1/auth/signup', 308)
-  async createUserV1(): Promise<void> {}
+  async createUserV1(): Promise<void> { }
 
-  // TODO: (param) user id
-  @Post(':followeeId/following')
+  // TODO: (param) followee alias
+  @Post(':followeeAlias/following')
   async followUser(): Promise<Api.HttpResponseBody<never>> {
     throw new NotImplementedException()
   }
