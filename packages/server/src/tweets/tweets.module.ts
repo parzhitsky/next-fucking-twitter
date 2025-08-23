637ac23cbc4470d58062e3ebca47c979db521d15
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2025-08-23 14:08:06 +0300

    Fix missing import

--- packages/server/src/tweets/tweets.module.ts
+++ packages/server/src/tweets/tweets.module.ts
@@ -1,8 +1,13 @@
 import { Module } from "@nestjs/common"
+import { DbModule } from "@/db/db.module.js"
 import { TweetsController } from "./tweets.controller.js"
+import { Tweet } from "./tweet.entity.js"
 import { TweetsService } from "./tweets.service.js"
 
 @Module({
+  imports: [
+    DbModule.forFeature([Tweet]),
+  ],
   providers: [
     TweetsService,
   ],
